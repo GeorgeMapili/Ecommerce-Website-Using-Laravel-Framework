960429fa8a12090d681c10b2acf5456e65a79b8d
raymart <tramyarnanulatgap@gmail.com>
2021-08-07 00:30:52 +0800

    add

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,14 +10,11 @@
 </head>
 <body>
 
-
 	@include('layouts.header')
 	
 	@yield('content')
 
 	@include('layouts.footer')
-
-
 
 <!-- Bootstra JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
